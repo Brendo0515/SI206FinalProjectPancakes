ed802a19ba89f627af1f07b07e0eb606c0db530a
Brendon Tseng <brendontseng@buschss-air.attlocal.net>
2019-04-26 19:42:52 -0400

    Documentation

--- Zomato_vis.py
+++ Zomato_vis.py
@@ -4,21 +4,23 @@ import sqlite3
 
 
 # read data from database
-conn = sqlite3.connect("pancakes.sqlite")
-cur = conn.cursor()
+def zomato_vis(db_filename):
+    conn = sqlite3.connect(db_filename)
+    cur = conn.cursor()
 
-#select all restaurant ratings
-cur.execute('SELECT Restaurant_Rating from Zomato')
-results = cur.fetchall()
-ratings = []
-for item in results:
-    ratings.append(item[0])
-cur.close()
+#   select all restaurant ratings
+    cur.execute('SELECT Restaurant_Rating from Zomato')
+    results = cur.fetchall()
+    ratings = []
+    for item in results:
+        ratings.append(item[0])
+    cur.close()
+    return ratings
     
 # plotted the times in a box plot to visualize the data
 fig = plt.figure()
 bp = fig.add_subplot(111)
-bp.boxplot(ratings)
+bp.boxplot(zomato_vis("pancakes.sqlite"))
 bp.set_ylabel('Ratings per Restaurant')
 bp.set_title("Box Plot of Restaurant Ratings")
 plt.show()
